3d0c2e7680a5bd18472c2ed96cd864bdbafc6f80
Michał Sitko <pierwszy1@gmail.com>
2020-02-13 14:58:57 +0100

    site directory added to git, travis verifies HTML instead of generating it

--- verifyHtml.sh
+++ verifyHtml.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script verifies whether html files generated out from dhall do not differ from ones in `site` directory
+# In other way it will not allow to manually edit html files but they're still as part of repository which
+# is useful because you can track how HTML changes over time. In particular it gives you ability to 
+# distinguish dhall refactoring, which does not changes html, from other changes
+
 # Exit when any command fails
 set -e
 
@@ -20,4 +25,6 @@ do
     $DHALL_BIN text <<< "./$f" > "./dhall/$filename.html"
     echo "Verify html: ./dhall/$filename.html vs ./site/$filename.html"
     diff "./dhall/$filename.html" "./site/$filename.html"
+
+    rm "./dhall/$filename.html"
 done
